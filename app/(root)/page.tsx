9ad79b9e2abd398d7d05fdb92db8294cc69608b9
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2023-09-04 14:22:57 +0300

    created admin dashboard

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+
+
+export default function Home() {
+    return (
+      <div className="p-4">
+        <Button size="default" >Click Me!</Button>
+      </div>
+    )
+  }
+  
