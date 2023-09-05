8c88d71ae2859faee7637b59d0c4cc898dd933cd
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2023-09-05 17:28:35 +0300

    Feat: store-switcher created

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,27 @@
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, auth } from "@clerk/nextjs";
 import React from "react";
-import MainNav from "@/components/main.nav";
-const Navbar = () => {
+import MainNav from "@/components/main-nav";
+import StoreSwitcher from "@/components/ui/store-switcher";
+import { redirect } from "next/navigation";
+import prismadb from "@/lib/prismadb";
+
+const Navbar = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const stores = await prismadb.store.findMany({
+    where: {
+      userId,
+    },
+  });
+
   return (
     <div className="flex px-4 border-b">
       <div className="flex h-16 items-center px-4 -ml-4">
-        <div>Stor Switcher</div>
+        <StoreSwitcher items={stores} />
       </div>
       <MainNav className="mx-6" />
       <div className="ml-auto flex items-center space-x-4">
