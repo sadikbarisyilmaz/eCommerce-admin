d06aa31d71a43d6b7b1150cdb157a0cb178df73a
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2023-09-05 13:57:29 +0300

    Feat: created store API

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ModalProvider } from "@/providers/modal-provider";
+import { ToasterProvider } from "@/providers/toast-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
       <html lang="en">
         <body>
           <ModalProvider />
+          <ToasterProvider />
           {children}
         </body>
       </html>
