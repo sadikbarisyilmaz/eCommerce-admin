d6ccde88ac75b6a6e2f56a7e39b2db4322553c13
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2023-09-08 19:17:24 +0300

    Feat: orders added

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -17,16 +17,6 @@ const MainNav = ({
       active: pathname === `/${params.storeId}`,
     },
     {
-      href: `/${params.storeId}/products`,
-      label: "Products",
-      active: pathname === `/${params.storeId}/products`,
-    },
-    {
-      href: `/${params.storeId}/settings`,
-      label: "Settings",
-      active: pathname === `/${params.storeId}/settings`,
-    },
-    {
       href: `/${params.storeId}/billboards`,
       label: "Billboards",
       active: pathname === `/${params.storeId}/billboards`,
@@ -45,6 +35,21 @@ const MainNav = ({
       href: `/${params.storeId}/colors`,
       label: "Colors",
       active: pathname === `/${params.storeId}/colors`,
+    },
+    {
+      href: `/${params.storeId}/products`,
+      label: "Products",
+      active: pathname === `/${params.storeId}/products`,
+    },
+    {
+      href: `/${params.storeId}/orders`,
+      label: "Orders",
+      active: pathname === `/${params.storeId}/orders`,
+    },
+    {
+      href: `/${params.storeId}/settings`,
+      label: "Settings",
+      active: pathname === `/${params.storeId}/settings`,
     },
   ];
   return (
